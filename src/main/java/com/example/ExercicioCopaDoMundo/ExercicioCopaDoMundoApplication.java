0c885fd9f3c6c498d81ce545ea0f3a9eb48f0154
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-06-08 08:31:03 -0300

    Initial commit

---
+++ src/main/java/com/example/ExercicioCopaDoMundo/ExercicioCopaDoMundoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExercicioCopaDoMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioCopaDoMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioCopaDoMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExercicioCopaDoMundo/ExercicioCopaDoMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExercicioCopaDoMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioCopaDoMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
