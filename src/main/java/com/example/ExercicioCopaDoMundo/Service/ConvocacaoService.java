1d1a60567715a9b2a1d8e680cb7981ccd66409df
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-06-09 16:23:36 -0300

    Service feito

--- src/main/java/com/example/ExercicioCopaDoMundo/Service/ConvocacaoService.java
+++ src/main/java/com/example/ExercicioCopaDoMundo/Service/ConvocacaoService.java
@@ -1,4 +1,27 @@
 package com.example.ExercicioCopaDoMundo.Service;
 
+import com.example.ExercicioCopaDoMundo.Model.Convocacao;
+import com.example.ExercicioCopaDoMundo.Model.Selecao;
+import com.example.ExercicioCopaDoMundo.Repository.ConvocacaoRepository;
+import com.example.ExercicioCopaDoMundo.Repository.SelecaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ConvocacaoService {
+
+    private final ConvocacaoRepository convocacaoRepository;
+    private final SelecaoRepository selecaoRepository;
+
+    public Convocacao registar(Convocacao convocacao){
+        Selecao selecao = selecaoRepository.findById(convocacao.getSelecao().getId())
+                .orElseThrow(() -> new RuntimeException("Seleção não encontrada"));
+        Integer saldoAtual = selecao.getJogadoresDisponiveis();
+        selecao.setJogadoresDisponiveis(saldoAtual + convocacao.getQuantidade());
+
+        selecaoRepository.save(selecao);
+        convocacao.setSelecao(selecao);
+        return convocacaoRepository.save(convocacao);
+    }
 }
--- src/main/java/com/example/ExercicioCopaDoMundo/Service/PartidaService.java
+++ src/main/java/com/example/ExercicioCopaDoMundo/Service/PartidaService.java
@@ -1,4 +1,31 @@
 package com.example.ExercicioCopaDoMundo.Service;
 
+import com.example.ExercicioCopaDoMundo.Model.Partida;
+import com.example.ExercicioCopaDoMundo.Model.Selecao;
+import com.example.ExercicioCopaDoMundo.Repository.PartidaRespository;
+import com.example.ExercicioCopaDoMundo.Repository.SelecaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PartidaService {
+
+    private final PartidaRespository partidaRespository;
+    private final SelecaoRepository selecaoRepository;
+
+    public Partida registrar(Partida partida) {
+        Selecao selecao = selecaoRepository.findById(partida.getSelecao().getId())
+                .orElseThrow(() -> new RuntimeException("Seleção não encontrada"));
+
+        if (selecao.getJogadoresDisponiveis() < partida.getQuantidade()) {
+            throw new RuntimeException("Quantidade indisponivel de jogadores");
+        }
+        selecao.setJogadoresDisponiveis(selecao.getJogadoresDisponiveis() - partida.getQuantidade());
+
+        selecaoRepository.save(selecao);
+        partida.setSelecao(selecao);
+        return partidaRespository.save(partida);
+
+    }
 }
